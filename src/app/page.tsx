fa6093ded94d8a5fbe42ca55024d6299ba88cb01
phao9350@gmail.com <phao9350@gmail.com>
2025-12-26 09:37:56 +0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,28 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import Image from "next/image";
+import Card from "./Card";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
+        {/* <Image
           className="dark:invert"
           src="/next.svg"
           alt="Next.js logo"
           width={100}
           height={20}
           priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+        /> */}
+        {/* <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+            Xin chào mn mình học nextJS nha
           </h1>
+          <Button className="p-10" variant={"outline"}>
+            Mua khóa học Next.js kìa haha
+          </Button>
+          <Input type="email" placeholder="Email" />
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
             <a
@@ -33,8 +40,8 @@ export default function Home() {
             </a>{" "}
             center.
           </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
+        </div> */}
+        {/* <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -58,7 +65,9 @@ export default function Home() {
           >
             Documentation
           </a>
-        </div>
+        </div> */}
+
+        <Card />
       </main>
     </div>
   );
